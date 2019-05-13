580b497e04a66bfed7252ddd30b1e693ffc752b6
Marnie Reid <marniejeanreid@gmail.com>
2019-05-12 20:03:07 -0700

    Complete Iteration 1

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func showMessage(sender: UIButton) {
+        
+        // in as much detail as possible
+        // pseudo-code the things that need to happen
+        // when a user clicks an emoji button
+      let alertController = UIAlertController(title: "Non-professional Med-emoji advice:", message: "Load up on Kleenex!", preferredStyle: UIAlertController.Style.alert)
+    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
