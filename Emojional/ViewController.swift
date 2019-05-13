bc555d8860e04c08ee7255a04722ad4ee30e5e69
Marnie Reid <marniejeanreid@gmail.com>
2019-05-12 19:08:39 -0700

    Initial Commit

---
+++ Emojional/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emojional
+//
+//  Created by Marnie Reid on 5/12/19.
+//  Copyright © 2019 Marnie Reid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
